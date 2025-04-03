1ac0bd12687ffea402cee0e54a94494730c0e9e8
adelinapetkova <stu2101261003@uni-plovdiv.bg>
2025-04-03 18:42:52 +0300

    Simulation functionality updates

--- transport_optimization/transport_optimization_app/simulation_handlers/SimulationHandler.py
+++ transport_optimization/transport_optimization_app/simulation_handlers/SimulationHandler.py
@@ -4,17 +4,19 @@ from .PassengerAgent import PassengerAgent
 
 class SimulationHandler:
     def __init__(self, routes_solution, num_passengers=1000, steps=10):
-        self.routes_solution = routes_solution
+        self.__routes_solution = routes_solution
         self.__num_passengers = num_passengers
         self.__steps = steps
 
     def run_simulation(self):
         """Start the simulation with a given number of passengers"""
-        model = TransportModel(self.__num_passengers, self.routes_solution)
+        model = TransportModel(self.__num_passengers, self.__routes_solution)
 
+        # Execute the simulation with different passengers
         for _ in range(self.__steps):
             model.step()
 
+        # Collect the results from all the passengers
         travel_distances = []
         travel_times = []
         transfers = []
@@ -25,10 +27,20 @@ class SimulationHandler:
                 travel_times.append(agent.travel_time)
                 transfers.append(agent.transfer_count)
 
+        # Calculate the score of the solution based on the average values for travel time, distance,
+        # transfers count and direct trips percentage
         avg_travel_distance = sum(travel_distances) / self.__num_passengers
         avg_travel_time = sum(travel_times) / self.__num_passengers
         avg_transfers = sum(transfers) / self.__num_passengers
         direct_trips_percentage = sum(1 for t in transfers if t == 0) / self.__num_passengers * 100
 
-        # TODO: Add some simulation evaluation formula here and return one value
+        # Higher score means better solution
+        score = (
+                (1 / avg_travel_distance) +
+                (1 / avg_travel_time) +
+                (1 / (1 + avg_transfers)) +
+                (direct_trips_percentage / 100)
+        )
+
+        return score
 
--- transport_optimization/transport_optimization_app/simulation_handlers/TransportModel.py
+++ transport_optimization/transport_optimization_app/simulation_handlers/TransportModel.py
@@ -11,8 +11,8 @@ class TransportModel(Model):
     """Transport network simulation model"""
 
     def __init__(self, num_passengers, routes_solution):
-        self.num_passengers = num_passengers
-        self.routes_solution = routes_solution
+        self.__num_passengers = num_passengers
+        self.__routes_solution = routes_solution
 
         self.schedule = RandomActivation(self)
         self.__times = [datetime.time(8, 0), datetime.time(12, 0), datetime.time(18, 0)]
@@ -27,7 +27,7 @@ class TransportModel(Model):
     def __create_passengers(self):
         """Create passengers by choosing random start and end stops"""
         stops = list(Stop.objects.all())
-        for i in range(0, self.num_passengers * len(self.__times), len(self.__times)):
+        for i in range(0, self.__num_passengers * len(self.__times), len(self.__times)):
             start_stop, end_stop = random.sample(stops, 2)
             # For each start and end stop combination we create passengers for three different times of the day
             for j in range(len(self.__times)):
@@ -35,7 +35,7 @@ class TransportModel(Model):
                 self.schedule.add(passenger)
 
     def __create_travel_time_dict(self):
-        """Preload data into a dictionary for fast lookups"""
+        """Preload DB data into a dictionary for fast lookups"""
         self.__travel_time_dict = {
             (start_stop, end_stop, time_of_day): (travel_time_seconds, distance_meters)
             for start_stop, end_stop, time_of_day, travel_time_seconds, distance_meters in
@@ -45,16 +45,20 @@ class TransportModel(Model):
 
     def __fill_routes_to_graph(self):
         """Built a graph for the given routes solution"""
-        for route, stops in self.routes_solution.items():
+        # Add graph score labels to dictionary
+        attr_name = "score_{}"
+        for t in range(len(self.__times)):
+            self.__time_to_graph_attr_map[str(self.__times[t])] = attr_name.format(t)
+
+        # Fill the graph with all the routes from the given solution
+        for route, stops in self.__routes_solution.items():
             for i in range(len(stops) - 1):
                 stop1, stop2 = stops[i], stops[i + 1]
                 for j in range(len(self.__times)):
                     travel_time, distance = self.__travel_time_dict.get((str(stop1), str(stop2), self.__times[j]),
                                                                         (None, None))
                     if travel_time and distance:
-                        attr_name = f"score_{j}"
-                        self.__time_to_graph_attr_map[str(self.__times[j])] = attr_name
-                        self.__graph.add_edge(stop1, stop2, {attr_name: travel_time + distance})
+                        self.__graph.add_edge(stop1, stop2, {attr_name.format(j): travel_time + distance})
 
     def step(self):
         """Execute one step of the simulation"""
@@ -64,8 +68,7 @@ class TransportModel(Model):
         """Get route travel time and distance for the given start and end stop"""
         start_index = route.index(start_stop)
         end_index = route.index(end_stop)
-        time = 0
-        dist = 0
+        time, dist = 0, 0
 
         for i in range(start_index, end_index):
             tt_data = self.__travel_time_dict.get((str(route[i]), str(route[i + 1]), time_of_the_day))
@@ -94,58 +97,55 @@ class TransportModel(Model):
             current_stop_index = path.index(current_stop)
             for i in range(current_stop_index + 1, len(path)):
                 next_stop = path[i]
-                current_route_stops = self.routes_solution[current_route]
+                current_route_stops = self.__routes_solution[current_route]
+
+                # If the current route includes also the next stop in the path we continue
                 if next_stop in current_route_stops \
                         and abs(current_route_stops.index(current_stop) - current_route_stops.index(next_stop)) == 1:
                     current_stop = next_stop
                     continue
+
+                # If the current route does not include the next stop a transfer will be needed
+                # and we need to check recursively all the possible routes we could take
                 transfers += 1
-                possible_next_routes = [r for r in self.routes_solution.keys()
-                                        if current_stop in self.routes_solution[r]
-                                        and next_stop in self.routes_solution[r]
-                                        and abs(self.routes_solution[r].index(current_stop)
-                                                - self.routes_solution[r].index(next_stop)) == 1]
+                possible_next_routes = [r for r in self.__routes_solution.keys()
+                                        if current_stop in self.__routes_solution[r]
+                                        and next_stop in self.__routes_solution[r]
+                                        and abs(self.__routes_solution[r].index(current_stop)
+                                                - self.__routes_solution[r].index(next_stop)) == 1]
                 transfers += self.__get_transfers_recursive(path, possible_next_routes, next_stop)
                 break
             if transfers < min_transfers:
                 min_transfers = transfers
         return min_transfers
 
-    def get_transfers_count_in_route(self, path):
+    def __get_transfers_count_in_route(self, path):
         """Get the number of transfers in a path"""
-        possible_start_routes = [r for r in self.routes_solution.keys()
-                                 if path[0] in self.routes_solution[r]
-                                 and path[1] in self.routes_solution[r]
-                                 and abs(self.routes_solution[r].index(path[0])
-                                         - self.routes_solution[r].index(path[1])) == 1]
+        possible_start_routes = [r for r in self.__routes_solution.keys()
+                                 if path[0] in self.__routes_solution[r]
+                                 and path[1] in self.__routes_solution[r]
+                                 and abs(self.__routes_solution[r].index(path[0])
+                                         - self.__routes_solution[r].index(path[1])) == 1]
 
         return self.__get_transfers_recursive(path, possible_start_routes, path[1])
 
     def find_best_route(self, start_stop, end_stop, time_of_the_day):
         """Find best route (direct or with transfer)"""
         # First we check if there is a direct route for the two stops and if there is we return it
-        routes = list(self.routes_solution.keys())
-        direct_routes = [r for r in routes if start_stop in self.routes_solution[r]
-                         and end_stop in self.routes_solution[r]]
+        routes = list(self.__routes_solution.keys())
+        direct_routes = [r for r in routes if start_stop in self.__routes_solution[r]
+                         and end_stop in self.__routes_solution[r]]
 
         if direct_routes:
-            best_route = min(direct_routes, key=lambda r: sum(self.__get_travel_time_and_distance(r,
-                                                                                                  start_stop,
-                                                                                                  end_stop,
-                                                                                                  time_of_the_day)))
+            best_route = min(direct_routes,
+                             key=lambda r: sum(self.__get_travel_time_and_distance(r, start_stop,
+                                                                                   end_stop, time_of_the_day)))
             return 0, *self.__get_travel_time_and_distance(best_route, start_stop, end_stop, time_of_the_day)
 
         # If there is no direct route find the shortest one in the graph
         best_route = self.__find_shortest_path(start_stop, end_stop, time_of_the_day)
 
         # Calculate the number of transfers in the best route
-        transfers_count = self.get_transfers_count_in_route(best_route)
+        transfers_count = self.__get_transfers_count_in_route(best_route)
 
-        # Calculate the total travel time and distance
-        # TODO
-
-        best_time = 0
-        best_distance = 0
-        min_transfers = 0
-
-        return min_transfers, best_time, best_distance
+        return transfers_count, *self.__get_travel_time_and_distance(best_route, start_stop, end_stop, time_of_the_day)
